19e9388f3127614d1c4d7e83e558e4098d92e3cf
Psychokiller1888 <laurentchervet@bluewin.ch>
2018-10-16 19:22:18 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 
 apt-get update
 apt-get install -y python-pip
+apt-get install -y mosquitto
+apt-get install -y mosquitto-clients
+
 pip install spidev
-pip install numpy
 pip install gpiozero
+pip install paho-mqtt
+pip install pytoml
+pip install numpy
 
 mkdir logs
 chown _snips logs
