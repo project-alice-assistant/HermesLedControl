d62799f86ceb90bd9f4b34e1cec8fc030414839c
Psychokiller1888 <laurentchervet@bluewin.ch>
2018-10-16 20:50:22 +0200

    crappy numpy

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ apt-get update
 apt-get install -y python-pip
 apt-get install -y mosquitto
 apt-get install -y mosquitto-clients
+apt-get install -y python-numpy
 
 pip install spidev
 pip install gpiozero
 pip install paho-mqtt
 pip install pytoml
-pip install numpy
 
 mkdir logs
 chown _snips logs
