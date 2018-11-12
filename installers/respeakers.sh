802cd43cf18501e2ae4d298d1a1c1b2034e7693e
Psychokiller1888 <laurentchervet@bluewin.ch>
2018-11-12 15:38:47 +0100

    Adding confirmation before removing snips-skill-respeaker

--- installers/respeakers.sh
+++ installers/respeakers.sh
@@ -4,9 +4,18 @@ echo "######################### Respeakers installation ########################
 echo "#################### Please run this script with sudo #####################"
 
 if [[ -d "/var/lib/snips/skills/snips-skill-respeaker" ]]; then
-    rm -rf "/var/lib/snips/skills/snips-skill-respeaker"
-    systemctl restart snips-*
-    echo "Removed snips-skill-respeaker"
+    echo "snips-skill-respeaker detected, do you want to remove it? Leaving it be might result in weird behaviors..."
+    select answer in "yes" "no" "cancel"; do
+        case $answer in
+            yes)
+                rm -rf "/var/lib/snips/skills/snips-skill-respeaker"
+                systemctl restart snips-*
+                echo "Removed snips-skill-respeaker"
+                break;;
+            cancel) exit;;
+            *) break;;
+        esac
+    done
 fi
 
 cd /home/pi
