f08563ccb9459cdfbd8fbd70f07a24a5adbf94f9
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-01-15 10:57:55 +0100

    New matrix voice installer. Untested, need testers

--- installers/matrixvoice.sh
+++ installers/matrixvoice.sh
@@ -3,15 +3,25 @@
 echo "######################## Matrix Voice installation ########################"
 echo "#################### Please run this script with sudo #####################"
 
-mkdir -p /home/pi/matrixvoice
-cd /home/pi/matrixvoice
-wget "https://github.com/matrix-io/matrix-creator-malos/blob/master/src/python_test/Pipfile" -O Pipfile
-wget "https://github.com/matrix-io/matrix-creator-malos/blob/master/src/python_test/Pipfile.lock" -O Pipfile.lock
-wget "https://raw.githubusercontent.com/matrix-io/matrix-creator-malos/master/src/python_test/requirements.txt" -O requirements.txt
-apt-get update
-apt-get install build-essential python-dev python-pip
-pip install -r requirements.txt
-rm -rf /home/pi/matrixvoice
+curl https://apt.matrix.one/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.matrix.one/raspbian $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/matrixlabs.list
+echo "deb http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/ ./" | sudo tee /etc/apt/sources.list.d/zeromq.list
+wget https://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/Release.key -O- | sudo apt-key add
+sudo apt-get update
+sudo apt-get install -y build-essential python-dev matrixio-malos
+
+pip --no-cache-dir install appdirs
+pip --no-cache-dir install backports-abc
+pip --no-cache-dir install certifi
+pip --no-cache-dir install matrix_io-proto
+pip --no-cache-dir install packaging
+pip --no-cache-dir install protobuf
+pip --no-cache-dir install pyparsing
+pip --no-cache-dir install pyzmq
+pip --no-cache-dir install zmq
+pip --no-cache-dir install singledispatch
+pip --no-cache-dir install six
+pip --no-cache-dir install tornado
 
 echo "######################## All done! ########################"
 echo "##################### Enjoy! Psycho #######################"
