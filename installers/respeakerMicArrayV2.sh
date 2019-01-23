9b860cf085bea4da715cd06ca08e7a87f5324772
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-01-23 18:11:37 +0100

    Fixing respeaker mic array v2 install, thank herrwusel

--- installers/respeakerMicArrayV2.sh
+++ installers/respeakerMicArrayV2.sh
@@ -16,7 +16,12 @@ cd /home/pi
 rm -rf /home/pi/usb_4_mic_array
 git clone https://github.com/respeaker/usb_4_mic_array.git
 cd usb_4_mic_array
-python dfu.py --download "$channels"_channels_firmware.bin
+
+if [ "channels" == 6 ]; then
+    python dfu.py --download 6_channels_firmware.bin
+else
+    python dfu.py --download 1_channel_firmware.bin
+fi
 
 echo "############################## All done! ##############################"
 echo "############## Don't forget to turn on the SPI interface! #############"
