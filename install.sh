0b1a4acecba21837671b2ecd8e72e66c9ecc6241
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-07-29 21:24:56 +0200

    cleanup

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ done
 
 if [[ "$device" != "don't overwrite existing parameters" ]]; then
     echo "What pattern do you want to use?"
-    select pattern in "google" "alexa" "custom" "kiboost" "cancel"; do
+    select pattern in "google" "alexa" "projectalice" "custom" "kiboost" "cancel"; do
         case "$pattern" in
             cancel) exit;;
             *) break;;
--- installers/neopixels.sh
+++ installers/neopixels.sh
@@ -5,15 +5,7 @@ echo "#################### Please run this script with sudo ####################
 USER=$(logname)
 
 sed -i -e "\$acore_freq=250" /boot/config.txt
-apt-get install scons
 apt-get install swig
-
-cd /home/${USER}
-#git clone https://github.com/jgarff/rpi_ws281x.git
-#cd rpi_ws281x
-#scons
-#cd python
-#python3.5 setup.py install
 
 pip3 install rpi-ws281x
 
