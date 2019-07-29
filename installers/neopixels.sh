abb753930b4df0bfee12e6a77d5d845722483a80
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-07-29 21:17:17 +0200

    rpi-ws281x is available on pip...

--- installers/neopixels.sh
+++ installers/neopixels.sh
@@ -9,11 +9,13 @@ apt-get install scons
 apt-get install swig
 
 cd /home/${USER}
-git clone https://github.com/jgarff/rpi_ws281x.git
-cd rpi_ws281x
-scons
-cd python
-python3.5 setup.py install
+#git clone https://github.com/jgarff/rpi_ws281x.git
+#cd rpi_ws281x
+#scons
+#cd python
+#python3.5 setup.py install
+
+pip3 install rpi-ws281x
 
 echo "###################################### All done! #######################################"
 echo "##################### Don't forget to turn on the SPI interface! #######################"
