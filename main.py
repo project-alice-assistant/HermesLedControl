c3c10549d7363628612e3f108c99bcc07b8c6580
Psycho <laurentchervet@bluewin.ch>
2019-08-03 07:26:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def onStop():
 
 
 def main():
-	_logger.info('Starting Snips Led Control v. 1.9.6')
+	_logger.info('Starting Snips Led Control v. 1.9.7')
 
 	signal.signal(signal.SIGINT, stopHandler)
 	signal.signal(signal.SIGTERM, stopHandler)
