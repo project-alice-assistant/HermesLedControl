8e6a4fb6446929e247197e9e47d455343ca0b613
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-08-06 23:04:10 +0200

    Some cleanup and fixes

--- install.sh
+++ install.sh
@@ -122,53 +122,43 @@ select answer in "yes" "no" "cancel"; do
         yes)
             case "$device" in
                 matrixvoice)
-                    chmod +x ./installers/matrixVoiceCreator.sh
                     ./installers/matrixVoiceCreator.sh ${USER} ${FVENV}
                     break
                     ;;
                 matrixcreator)
-                    chmod +x ./installers/matrixVoiceCreator.sh
                     ./installers/matrixVoiceCreator.sh ${USER} ${FVENV}
                     break
                     ;;
                 respeaker2)
-                    chmod +x ./installers/respeakers.sh ${USER} ${FVENV}
-                    ./installers/respeakers.sh
+                    ./installers/respeakers.sh ${USER} ${FVENV}
                     break
                     ;;
                 respeaker4)
-                    chmod +x ./installers/respeakers.sh ${USER} ${FVENV}
-                    ./installers/respeakers.sh
+                    ./installers/respeakers.sh ${USER} ${FVENV}
                     break
                     ;;
                 respeaker6MicArray)
-                    chmod +x ./installers/respeakers.sh ${USER} ${FVENV}
-                    ./installers/respeakers.sh
+                    ./installers/respeakers.sh ${USER} ${FVENV}
                     break
                     ;;
                 neoPixelsSK6812RGBW)
-                    chmod +x ./installers/neopixels.sh ${USER} ${FVENV}
-                    ./installers/neopixels.sh
+                    ./installers/neopixels.sh ${USER} ${FVENV}
                     break
                     ;;
                 neoPixelsWS2812RGB)
-                    chmod +x ./installers/neopixels.sh ${USER} ${FVENV}
-                    ./installers/neopixels.sh
+                    ./installers/neopixels.sh ${USER} ${FVENV}
                     break
                     ;;
                 respeakerMicArrayV2)
-                    chmod +x ./installers/respeakerMicArrayV2.sh ${USER} ${FVENV}
-                    ./installers/respeakerMicArrayV2.sh
+                    ./installers/respeakerMicArrayV2.sh ${USER} ${FVENV}
                     break
                     ;;
                 respeakerCoreV2)
-                    chmod +x ./installers/respeakerCoreV2.sh ${USER} ${FVENV}
-                    ./installers/respeakerCoreV2.sh
+                    ./installers/respeakerCoreV2.sh ${USER} ${FVENV}
                     break
                     ;;
                 respeaker7MicArray)
-                    chmod +x ./installers/respeaker7MicArray.sh ${USER} ${FVENV}
-                    ./installers/respeaker7MicArray.sh
+                    ./installers/respeaker7MicArray.sh ${USER} ${FVENV}
                     break
                     ;;
                 *)
--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def onStop():
 
 
 def main():
-	_logger.info('Starting Snips Led Control v. 1.9.7')
+	_logger.info('Starting Snips Led Control v. 1.9.8')
 
 	signal.signal(signal.SIGINT, stopHandler)
 	signal.signal(signal.SIGTERM, stopHandler)
