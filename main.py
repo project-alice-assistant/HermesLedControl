545bde3da9c6ec6fc8d626e9660eadbba1cc9537
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-08-12 19:23:46 +0200

    Tag 1.9.9

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def onStop():
 
 
 def main():
-	_logger.info('Starting Snips Led Control v. 1.9.8')
+	_logger.info('Starting Snips Led Control v. 1.9.9')
 
 	signal.signal(signal.SIGINT, stopHandler)
 	signal.signal(signal.SIGTERM, stopHandler)
