c8b42abacdc150098db730da32c9e2fb69a58e05
Psychokiller1888 <laurentchervet@bluewin.ch>
2019-08-13 22:47:05 +0200

    Missing subscriptions

--- models/SnipsLedControl.py
+++ models/SnipsLedControl.py
@@ -215,6 +215,8 @@ class SnipsLedControl:
 			(self._SUB_ON_SAY, 0),
 			(self._SUB_ON_THINK, 0),
 			(self._SUB_ON_LISTENING, 0),
+			(self._SUB_ON_PLAY_FINISHED, 0),
+			(self._SUB_ON_TTS_FINISHED, 0),
 			(self._SUB_ON_LEDS_TOGGLE_ON, 0),
 			(self._SUB_ON_LEDS_TOGGLE_OFF, 0),
 			(self._SUB_ON_LEDS_TOGGLE, 0),
