297e251af8b797bab170ac64b9ad7071f1623cc2
Psychokiller1888 <laurentchervet@bluewin.ch>
2021-12-05 14:18:01 +0100

    Allow version specifications as argument for HLC downloader

--- hlc_download.sh
+++ hlc_download.sh
@@ -7,14 +7,23 @@ fi
 
 apt-get install git
 
-latest=$(curl --silent "https://api.github.com/repos/project-alice-assistant/HermesLedControl/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+if [[ -z "$1" ]]; then
+    echo "No version supplied"
+    latest=$(curl --silent "https://api.github.com/repos/project-alice-assistant/HermesLedControl/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    echo "Will download $latest"
+    VERSION=$latest
+else
+    VERSION=$1
+    echo "Supplied version $VERSION"
+fi
+
 dest="/home/$(logname)/HermesLedControl"
 
 rm -rf "$dest"
 git clone https://github.com/project-alice-assistant/HermesLedControl.git "$dest"
 cd "$dest" || exit
 git fetch
-git checkout tags/"$latest"
+git checkout "$VERSION"
 git pull
 chown -R "$(logname)" "$dest"
 
