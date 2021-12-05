ef155dbecd89562e4dfa3f1f0cd8594e8aed5910
Psychokiller1888 <laurentchervet@bluewin.ch>
2021-12-05 06:07:56 +0100

    wip downloader

--- hlc_download.sh
+++ hlc_download.sh
@@ -5,8 +5,11 @@ if [[ "$EUID" -ne 0 ]]
   exit
 fi
 
-url=$(curl --silent "https://api.github.com/repos/project-alice-assistant/HermesLedControl/releases/latest" | grep -Po '"tarball_url": "\K.*?(?=")')
+apt-get install git
 latest=$(curl --silent "https://api.github.com/repos/project-alice-assistant/HermesLedControl/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+
+url=$(curl --silent "https://api.github.com/repos/project-alice-assistant/HermesLedControl/releases/latest" | grep -Po '"tarball_url": "\K.*?(?=")')
+
 
 path='/home/'$(logname)
 dest=${path}/hermesLedControl_${latest}
